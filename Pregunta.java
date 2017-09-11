6072375633d48ffbc2f005897b9d94fabd513b55
José Luis <jlfmtz_96@alumnos.uaslp.edu.mx>
2017-09-11 11:04:53 -0500

    Intercambio inicial de proyecto

---
+++ Pregunta.java
@@ -0,0 +1,25 @@
+public class Pregunta
+{
+    private String texto;
+    private String respuesta;
+    
+    public String asignarTexto(String pregunta)
+    {
+        
+    }
+    
+    public String asignarRespuesta(String res)
+    {
+        
+    }
+    
+    public boolean checarRespuesta(String res)
+    {
+        
+    }
+    
+    public void imprimete()
+    {
+        
+    }
+}
